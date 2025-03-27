1b9f9ed5586a9f4d2028290a1fa686c1c9017f3c
jericodejesus <dejesusjerico528@gmail.com>
2025-03-28 05:26:50 +0800

    Updated TreatmentAreaScreen: Moved back button to AppBar, removed FloatingActionButton

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -157,6 +157,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ),
 
+
                         Text(
                           "NURSE APP",
 
